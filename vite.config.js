9de9525e39b02324bfe70335944c2e7d6e3feef5
fraterr <practicalhumanism@gmail.com>
2026-04-14 00:08:24 +0200

    2

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   // Base should be your repository name for GitHub Pages
   // Example: '/i-ching-oracle/'
-  base: './',
+  base: '/YiKingOracle/',
   build: {
     outDir: 'dist',
   }
